36698f03f2889c9d2176a608d1b658194988db41
Nolan Kyhl <nolan@kyhl.com>
2025-01-05 20:42:23 -0600

    Initial Commit

---
+++ VenmoLauncher/VenmoLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenmoLauncherApp.swift
+//  VenmoLauncher
+//
+//  Created by Nolan Kyhl on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct VenmoLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
